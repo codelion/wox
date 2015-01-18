f12a36d783d02cbff5442ee9cd86966dc4386e38
Asankhaya Sharma <asankhaya@yahoo.com>
2015-01-18 12:06:28 +0800

    Add property based tests

---
+++ java/src/test/java/wox/serial/tests/ElementGenerator.java
@@ -0,0 +1,54 @@
+/*
+ * © Copyright 2015 -  SourceClear Inc
+ */
+
+package wox.serial.tests;
+
+import com.pholser.junit.quickcheck.generator.GenerationStatus;
+import com.pholser.junit.quickcheck.generator.Generator;
+import com.pholser.junit.quickcheck.generator.java.lang.StringGenerator;
+import com.pholser.junit.quickcheck.random.SourceOfRandomness;
+import java.util.List;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.jdom2.Element;
+
+/**
+ *
+ */
+public class ElementGenerator extends Generator<Element> {
+
+  ///////////////////////////// Class Attributes \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+  ////////////////////////////// Class Methods \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+  //////////////////////////////// Attributes \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+  /////////////////////////////// Constructors \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+  
+  public ElementGenerator() {
+    super(Element.class);
+  }
+  
+  ////////////////////////////////// Methods \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+  //------------------------ Implements:
+
+  //------------------------ Overrides: generate
+  
+  @Override
+  public Element generate(SourceOfRandomness rand, GenerationStatus gs) {
+    Element e = new Element(RandomStringUtils.randomAlphabetic(16));
+    int numofAttr = rand.nextInt(8);
+    for(int i=0; i<numofAttr;i++){
+      e.setAttribute(RandomStringUtils.randomAlphabetic(8),RandomStringUtils.randomAlphabetic(8));
+    }
+    e.addContent(RandomStringUtils.randomAlphabetic(rand.nextInt(16)));
+    return e;
+  }
+  
+  //---------------------------- Abstract Methods -----------------------------
+
+  //---------------------------- Utility Methods ------------------------------
+
+  //---------------------------- Property Methods -----------------------------
+}
---
+++ java/src/test/java/wox/serial/tests/PropertyJUnitTest.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wox.serial.tests;
+
+import org.junit.contrib.theories.*;
+import org.junit.runner.RunWith;
+import org.junit.Assert;
+import com.pholser.junit.quickcheck.ForAll;
+import com.pholser.junit.quickcheck.From;
+import wox.serial.EncodeBase64;
+import static org.hamcrest.Matchers.*;
+import org.jdom2.Element;
+import static org.junit.Assume.assumeThat;
+import static wox.serial.XMLUtil.element2String;
+
+/**
+ *
+ * @author asankhaya
+ */
+@RunWith(Theories.class)
+public class PropertyJUnitTest {
+  @Theory public void testEncodeBase64(@ForAll byte [] src){
+    byte [] ec = EncodeBase64.encode(src);
+    byte [] dec = EncodeBase64.decode(ec);
+    Assert.assertArrayEquals(src,dec);
+  }
+  
+  @Theory public void testEncodeBase64withLength(@ForAll byte [] src){
+    assumeThat(src.length, greaterThan(32)); 
+    byte [] ec = EncodeBase64.encode(src);
+    byte [] dec = EncodeBase64.decode(ec);
+    Assert.assertArrayEquals(src,dec);
+  }
+  
+  @Theory public void testElement2String(@ForAll @From(ElementGenerator.class) Element e) throws Exception {
+    String s = element2String(e);
+  }
+}
